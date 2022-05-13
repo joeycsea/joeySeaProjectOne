a1c24ca757c11be61aa1d85e3b5ea36af525027e
Joey Sea <joey.c.sea@gmail.com>
2022-05-12 20:48:25 -0400

    added button alert JS

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// add js code here
+// Book a party button alert
+
+const section = document.getElementsByClassName('hostAParty');
+const findOutMoreButton = section[0].children[2];
+
+findOutMoreButton.addEventListener('click', function(){
+   alert('Call 555-555-5555 to book your party!');
+});
+
+// Modal appears (coupon) when scrolled halfway (scroll spy) 
+
+
+
+
+// Add a comment in blog
