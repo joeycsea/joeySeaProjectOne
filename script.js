d21a7f06d6415b9641e995978bedd0c072032daa
Joey Sea <joey.c.sea@gmail.com>
2022-05-13 20:10:38 -0400

    added JS modal and hamburger menu functionality

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-// Book a party button alert
-
+// === Modal appears (Book a Party) when they click button ===
+const modal = document.querySelector('.modal');
 const section = document.getElementsByClassName('hostAParty');
 const findOutMoreButton = section[0].children[2];
+const closeButton = document.querySelector('.closeButton');
+const overlay = document.getElementById('overlay');
 
-findOutMoreButton.addEventListener('click', function(){
-   alert('Call 555-555-5555 to book your party!');
+function toggleModal () {
+   modal.classList.toggle('modal-active');
+   overlay.classList.toggle('overlay-active');
+}
+
+findOutMoreButton.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+
+
+
+
+
+
+// === Hamburger Slide-out Menu ===
+const menuButton = document.getElementsByClassName('slidingMenuButton');
+const slidingMenuButton = menuButton[0].children[0];
+
+const slidingMenu = document.getElementsByClassName('slidingMenu');
+const slidingMenuSlide = slidingMenu[0];
+
+slidingMenuButton.addEventListener('click', function(){
+   slidingMenuSlide.classList.toggle('reveal-active');
 });
-
-// Modal appears (coupon) when scrolled halfway (scroll spy) 
-
-
-
-
-// Add a comment in blog
